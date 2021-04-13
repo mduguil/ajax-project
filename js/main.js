7f4f43a85a25a4541554e4b153696a9381e29cc2
Mikaela Duguil <kimmieduggz@gmail.com>
2021-04-12 23:24:09 -0700

    api request

---
+++ js/main.js
@@ -0,0 +1,32 @@
+var dataReq1 = new XMLHttpRequest();
+dataReq1.open('GET', 'https://api.adviceslip.com/advice');
+dataReq1.responseType = 'text';
+dataReq1.onreadystatechange = function () {
+  if (this.readyState === 4 && this.status === 200) {
+    JSON.parse(this.responseText);
+  }
+};
+
+dataReq1.send();
+
+var dataReq2 = new XMLHttpRequest();
+dataReq2.open('GET', 'https://api.quotable.io/random');
+dataReq2.responseType = 'text';
+dataReq2.onreadystatechange = function () {
+  if (this.readyState === 4 && this.status === 200) {
+    JSON.parse(this.responseText);
+  }
+};
+
+dataReq2.send();
+
+var dataReq3 = new XMLHttpRequest();
+dataReq3.open('GET', 'https://dog.ceo/api/breeds/image/random');
+dataReq3.responseType = 'text';
+dataReq3.onreadystatechange = function () {
+  if (this.readyState === 4 && this.status === 200) {
+    JSON.parse(this.responseText);
+  }
+};
+
+dataReq3.send();
